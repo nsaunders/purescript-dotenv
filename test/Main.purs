39c8b9a263d41f586b1d32c9f0a7ab002deaefee
Nick Saunders <nick@saunde.rs>
2019-06-08 07:57:09 -0600

    0.13 updates (#21)
    Upgrades to support PureScript 0.13.

--- test/Main.purs
+++ test/Main.purs
@@ -2,14 +2,15 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (runSpec)
 import Test.Load as Load
 import Test.Parse as Parse
 import Test.Resolve as Resolve
 
 main :: Effect Unit
-main = run [ consoleReporter ] do
+main = launchAff_ $ runSpec [ consoleReporter ] do
   Load.tests
   Parse.tests
   Resolve.tests
