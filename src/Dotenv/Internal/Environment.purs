2bb2e4904129571bf18518397641d8de10bd7e36
Nick Saunders <nick@saunde.rs>
2019-06-20 18:33:57 -0700

    Remove unnecessary GetEnv operation from Environment module.

--- src/Dotenv/Internal/Environment.purs
+++ src/Dotenv/Internal/Environment.purs
@@ -5,15 +5,13 @@ import Data.Maybe (Maybe)
 import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
-import Foreign.Object (Object)
-import Node.Process (getEnv, lookupEnv, setEnv) as P
+import Node.Process (lookupEnv, setEnv) as P
 import Run (FProxy, Run, lift)
 
 type ENVIRONMENT = FProxy (EnvironmentF)
 
 data EnvironmentF a
-  = GetEnv (Object String -> a)
-  | LookupEnv String (Maybe String -> a)
+  = LookupEnv String (Maybe String -> a)
   | SetEnv String String a
 
 derive instance functorEnvironmentF :: Functor EnvironmentF
@@ -23,18 +21,12 @@ _environment = SProxy :: SProxy "environment"
 handleEnvironment :: EnvironmentF ~> Aff
 handleEnvironment op = liftEffect $
   case op of
-    GetEnv callback -> do
-      env <- P.getEnv
-      pure $ callback env
     LookupEnv name callback -> do
       value <- P.lookupEnv name
       pure $ callback value
     SetEnv name value next -> do
       P.setEnv name value
       pure next
-
-getEnv :: forall r. Run (environment :: ENVIRONMENT | r) (Object String)
-getEnv = lift _environment (GetEnv identity)
 
 lookupEnv :: forall r. String -> Run (environment :: ENVIRONMENT | r) (Maybe String)
 lookupEnv name = lift _environment (LookupEnv name identity)
