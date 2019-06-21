0e3878ef09c28240c865dc78854ae4c86ed1539a
Nick Saunders <nick@saunde.rs>
2019-06-20 18:31:01 -0700

    Clean up Resolve module.

--- src/Dotenv/Internal/Resolve.purs
+++ src/Dotenv/Internal/Resolve.purs
@@ -1,11 +1,9 @@
 -- | This module contains the logic for resolving `.env` values.
 
-module Dotenv.Internal.Resolve (values) where
+module Dotenv.Internal.Resolve where
 
 import Prelude
-import Control.Alt ((<|>))
 import Data.Array (unzip, zip)
-import Data.Bifunctor (rmap)
 import Data.Foldable (find)
 import Data.Maybe (Maybe(..))
 import Data.String (joinWith)
@@ -13,15 +11,15 @@ import Data.Traversable (sequence, traverse)
 import Data.Tuple (Tuple(..), fst, snd)
 import Dotenv.Internal.ChildProcess (CHILD_PROCESS, spawn)
 import Dotenv.Internal.Environment (ENVIRONMENT, lookupEnv)
-import Dotenv.Internal.Types (Environment, Name, Settings, Value(..))
-import Dotenv.Types (Settings) as Public
-import Foreign.Object (lookup)
+import Dotenv.Internal.Types (ResolvedValue, Setting, UnresolvedValue(..))
 import Run (Run)
 
+-- | A row that tracks the effects involved in value resolution
 type Resolution r = (childProcess :: CHILD_PROCESS, environment :: ENVIRONMENT | r)
 
-resolveValue' :: forall r. Settings -> Value -> Run (Resolution r) (Maybe String)
-resolveValue' settings = case _ of
+-- | Resolves a value according to its expression.
+resolveValue :: forall r. Array (Setting UnresolvedValue) -> UnresolvedValue -> Run (Resolution r) ResolvedValue
+resolveValue settings = case _ of
   LiteralValue value ->
     pure $ Just value
   CommandSubstitution cmd args -> do
@@ -35,36 +33,17 @@ resolveValue' settings = case _ of
       Nothing -> do
         case (snd <$> find (eq var <<< fst) settings) of
           Just unresolvedValue ->
-            resolveValue' settings unresolvedValue
+            resolveValue settings unresolvedValue
           Nothing ->
             pure Nothing
   ValueExpression unresolvedValues -> do
-    resolvedValues <- traverse (resolveValue' settings) unresolvedValues
+    resolvedValues <- traverse (resolveValue settings) unresolvedValues
     pure $ joinWith "" <$> sequence resolvedValues
 
-resolveValues' :: forall r. Settings -> Run (Resolution r) Public.Settings
-resolveValues' settings =
+-- | Resolves the values within an array of settings.
+resolveValues :: forall r. Array (Setting UnresolvedValue) -> Run (Resolution r) (Array (Setting ResolvedValue))
+resolveValues settings =
   let
-    (Tuple names values) = unzip settings
+    (Tuple names unresolvedValues) = unzip settings
   in
-    zip names <$> traverse (resolveValue' settings) values
-
--- | Given the environment and an array of `.env` settings, resolves the specified value.
-value
-  :: Environment
-  -> Settings
-  -> Value
-  -> Maybe String
-value env settings val =
-  let
-    value' = value env settings
-  in
-    case val of
-      LiteralValue v            -> pure v
-      CommandSubstitution c _   -> pure c
-      ValueExpression vs        -> joinWith "" <$> (sequence $ value' <$> vs)
-      VariableSubstitution name -> lookup name env <|> (value' =<< snd <$> find (eq name <<< fst) settings)
-
--- | Given the environment and an array of `.env` settings, resolves the value of each setting.
-values :: Environment -> Settings -> Public.Settings
-values env settings = rmap (value env settings) <$> settings
+    zip names <$> traverse (resolveValue settings) unresolvedValues
