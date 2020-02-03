c04ac4cd6de328ba83405b55fd91109217fdd950
Nick Saunders <nick@saunde.rs>
2020-02-03 06:29:21 -0700

    Add loadContents example.

---
+++ example/Contents.purs
@@ -0,0 +1,14 @@
+module Example.Contents where
+
+import Prelude
+import Dotenv (loadContents) as Dotenv
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console (logShow)
+import Node.Process (lookupEnv)
+
+main :: Effect Unit
+main = launchAff_ do
+  _ <- Dotenv.loadContents """GREETING="Hello, Sailor!" """
+  liftEffect $ lookupEnv "GREETING" >>= logShow
